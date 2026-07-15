d7773f56cd1579d68836c19ecae651075dc4132e
Anirudh Konidala <anirudhkonidala@gmail.com>
2026-07-14 20:05:26 -0500

    week 08: day 1 prob set 1 WIP

--- week-08/day-01/prob-set-01/prob03.py
+++ week-08/day-01/prob-set-01/prob03.py
@@ -1,2 +1,20 @@
-def prob03(root):
-    pass
+from references.treenode import TreeNode
+
+# Input: Binary Tree
+# Output: the right values of a Binary Tree
+def prob03(root: TreeNode | None) -> list[str]:
+    if not root:
+        return []
+
+    res = [root.val]
+    
+    def prob03_helper(node: TreeNode | None) -> list[str]:
+        if not node.right:
+            return res
+
+        prob03_helper(node.right)
+
+    return prob03_helper(root)
+
+# Time:  O(h) - visits one node per level down the right spine (h = height; O(log n) if balanced)
+# Space: O(h) - recursive: call stack depth h.
--- week-08/day-01/prob-set-01/prob06.py
+++ week-08/day-01/prob-set-01/prob06.py
@@ -1,2 +1,23 @@
-def prob06(root):
-    pass
+from references import TreeNode
+
+# Input: A Binary Tree that we need to do postorder traversal on
+# Output: A list of strings that represent the postorder traversal of the binary tree
+def prob06(root: TreeNode | None) -> list[str]:
+	if not root:
+		return []
+	
+	res = []
+	
+	def prob06_helper(node: TreeNode | None):
+		if not node:
+			return
+	
+		prob06_helper(node.left)
+		prob06_helper(node.right)
+		res.append(node.val)
+	
+	prob06_helper(root)
+	return res
+	
+# Time: O(n) - every node is visited exactly once
+# Space: O(h) - the recursion call stack is as deep as the tree's height h
